624499d0d3fa06a384e0277322e9a772ded5cb93
Jan Přikryl <jan.prikryl@itixo.com>
2025-10-19 22:50:22 +0200

    Removed unnecessary comments

--- client/src/utils/constants.rs
+++ client/src/utils/constants.rs
@@ -1,10 +1,10 @@
 pub const COLLECTION_COLORS: [&str; 8] = [
-    "#3B82F6", // modrá
-    "#A855F7", // fialová
-    "#EC4899", // růžová
-    "#10B981", // zelená
-    "#F59E0B", // oranžová
-    "#EF4444", // červená
-    "#38BDF8", // tyrkys
-    "#6366F1", // indigo
+    "#3B82F6",
+    "#A855F7",
+    "#EC4899",
+    "#10B981",
+    "#F59E0B",
+    "#EF4444",
+    "#38BDF8",
+    "#6366F1",
 ];
